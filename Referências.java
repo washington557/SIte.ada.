c97f39084425394bd0abdb695d02050a3fda6a87
Gui-PCS <69014770+Gui-PCS@users.noreply.github.com>
2021-04-09 23:24:30 -0300

    Update Referências.java

--- Referências.java
+++ Referências.java
@@ -1,3 +1,17 @@
 imagem backgroud pag2-https://www.ctrlplay.com.br/wp-content/uploads/2019/08/Ada-WomenThatDid.jpg
 https://darkside.blog.br/ada-lovelace-desafiou-o-machismo-e-se-tornou-a-primeira-programadora-da-historia/
 https://proec.ufabc.edu.br/gec/profissao-cientista/ada-lovelace/#:~:text=Ada%20Lovelace%2C%20chamada%20por%20Babbage,de%20f%C3%A1rmacos%20na%20mente%20humana.
+
+imagem backgroud pag6-https://www.ctrlplay.com.br/wp-content/uploads/2019/08/Ada-WomenThatDid.jpg
+https://wesinalves.github.io/biography/2019/03/08/ada-lovelace.html
+https://blog.locaweb.com.br/artigos/6-curiosidades-sobre-ada-lovelace-que-voce-precisa-saber/
+https://www.imdb.com/title/tt0118882/
+https://www.ruby-lang.org/pt/about/
+https://lpunb.fandom.com/wiki/Linguagem_ADA_-_Grupo_2
+https://1.bp.blogspot.com/-1nUT6lfTLkE/WEwhmbfLmKI/AAAAAAAAdMI/yfg_S2k4NdgzzEy1EM5KesXhRRGjZFcKgCLcB/s640/Ada_Lovelace_child_portrait_Somerville_College.jpg
+https://blogs.gre.ac.uk/diversity/wp-content/uploads/sites/11/2016/02/Mary-Somerville.png
+https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Ada_Lovelace_color.svg/468px-Ada_Lovelace_color.svg.png
+https://img2.gratispng.com/20180714/xx/kisspng-adafruit-industries-ada-lovelace-first-computer-p-charles-babbage-image-5b4aadfc8134a2.0480975615316208605292.jpg
+http://www.turfebrasil.not.br/dulce_07.jpg
+https://www.blogs.unicamp.br/cienciapelosolhosdelas/wp-content/uploads/sites/53/2016/09/adalovelaceday.jpg
+https://flxt.tmsimg.com/assets/p66315_v_v9_aa.jpg
