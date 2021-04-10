220aad3ada97b1c1da79658f0f7129542f74556f
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2021-04-09 23:22:25 -0300

    Update Referências.java

--- Referências.java
+++ Referências.java
@@ -1,1 +1,3 @@
 imagem backgroud pag2-https://www.ctrlplay.com.br/wp-content/uploads/2019/08/Ada-WomenThatDid.jpg
+https://darkside.blog.br/ada-lovelace-desafiou-o-machismo-e-se-tornou-a-primeira-programadora-da-historia/
+https://proec.ufabc.edu.br/gec/profissao-cientista/ada-lovelace/#:~:text=Ada%20Lovelace%2C%20chamada%20por%20Babbage,de%20f%C3%A1rmacos%20na%20mente%20humana.
